8b6a9f8dd6ae1a82db1088d33047f9c79d04639f
Gregg Carey <gregg.carey@outlook.com>
2022-04-05 16:24:43 -0500

    Removed comments and notes

--- CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
+++ CreditCardApplications.Tests/CreditCardApplicationEvaluatorShould.cs
@@ -21,7 +21,8 @@ namespace CreditCardApplications.Tests
         public void ReferYoungApplications()
         {
             var mockValidator = new Mock<IFrequentFlyerNumberValidator>();
-            mockValidator.DefaultValue = DefaultValue.Mock;//This value will mock any underlying reference values so they are not null. This can be a problem 
+     
+            mockValidator.DefaultValue = DefaultValue.Mock;
 
             CreditCardApplicationEvaluator sut = new(mockValidator.Object);
 
@@ -36,11 +37,7 @@ namespace CreditCardApplications.Tests
         [Fact]
         public void DeclineLowIncomeApplicaitons()
         {
-            /* Without proper mock setup, this test fails because the the Frequent Flyer IsValid check happens before 
-             * the Low Income Check, which causes an early return with an incorrect decesion. The .Setup method marks the 
-             * IsValid property as true, which satisfies the validation check
-             * if the FFNumber is different from the .Setup, the Validation will fail
-             */
+            
             Mock<IFrequentFlyerNumberValidator> mockValidator = new();
             mockValidator.Setup(x => x.ServiceInformation.License.LicenseKey).Returns("OK");
 
@@ -62,19 +59,12 @@ namespace CreditCardApplications.Tests
         [Fact]
         public void DeclineLowIncomeApplicaitons_ArgumentMatching()
         {
-            /* Without proper mock setup, this test fails because the the Frequent Flyer IsValid check happens before 
-             * the Low Income Check, which causes an early return with an incorrect decesion. The .Setup method marks the 
-             * IsValid property as true, which satisfies the validation check
-             * if the FFNumber is different from the .Setup, the Validation will fail
-             */
+           
             Mock<IFrequentFlyerNumberValidator> mockValidator = new();
             mockValidator.Setup(x => x.ServiceInformation.License.LicenseKey).Returns("OK");
 
-            //mockValidator.Setup(x => x.IsValid(It.IsAny<string>())).Returns(true); //Takes any string
-            //mockValidator.Setup(x => x.IsValid(It.Is<string>(number => number.StartsWith("y")))).Returns(true);// takes a string starting with y
-            //mockValidator.Setup(x => x.IsValid(It.IsInRange<string>("a", "z", Moq.Range.Inclusive))).Returns(true);// Range
-            //mockValidator.Setup(x => x.IsValid(It.IsIn("a", "z", "x"))).Returns(true);//for Sets
-            mockValidator.Setup(x => x.IsValid(It.IsRegex("[a-z]"))).Returns(true);//for Regex
+
+            mockValidator.Setup(x => x.IsValid(It.IsRegex("[a-z]"))).Returns(true);
 
             var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
 
@@ -94,59 +84,26 @@ namespace CreditCardApplications.Tests
         {
             Mock<IFrequentFlyerNumberValidator> mockValidator = new();
             mockValidator.Setup(x => x.ServiceInformation.License.LicenseKey).Returns("OK");
-            //With MockBehavior.Strict, any propertly access that is not explicitly set up 
-            //fail with an exception. Setting it up here will pass the test.
+      
             mockValidator.Setup(x => x.IsValid(It.IsAny<string>())).Returns(false);
 
             var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
             var application = new CreditCardApplication();
             CreditCardApplicationDecision decesion = sut.Evaluate(application);
 
-            //With MockBehavior.Loose (default) this test passed becuase of the early evaluation
-            //of Frequent Flyer validation number, even if that isn't the correct outcome
+
             Assert.Equal(CreditCardApplicationDecision.ReferredToHuman, decesion);
 
         }
-        //[Fact]
-        //public void DeclineLowIncomeApplicationsOutDemo()
-        //{
-        //    Mock<IFrequentFlyerNumberValidator> mockValidator = new();
-
-        //    bool isValid = true;
-        //    mockValidator.Setup(x => x.IsValid(It.IsAny<string>(), out isValid));
-
-        //    var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
-
-        //    var application = new CreditCardApplication
-        //    {
-        //        GrossAnnualIncome = 19_999,
-        //        Age = 42
-        //    };
-
-        //    CreditCardApplicationDecision decesion = sut.EvaluateUsingOut(application);
-
-        //    Assert.Equal(CreditCardApplicationDecision.AutoDeclined, decesion);
-        //}
+    
         [Fact]
         public void ReferWhenLicenseKeyExpired()
         {
-           
-
-          
-            /*Setup every property from parent to child
-            var mockLicenseData = new Mock<ILicenseData>();
-            mockLicenseData.Setup(x => x.LicenseKey).Returns("EXPIRED");
-            var mockServiceInfo = new Mock<IServiceInformation>();
-            mockServiceInfo.Setup(x => x.License).Returns(mockLicenseData.Object);
-            var mockValidator = new Mock<IFrequentFlyerNumberValidator>();
-            mockValidator.Setup(x => x.ServiceInformation).Returns(mockServiceInfo.Object);
-            */
+        
             var mockValidator = new Mock<IFrequentFlyerNumberValidator>();
             mockValidator.Setup(x => x.ServiceInformation.License.LicenseKey).Returns("EXPIRED");// less verbose
 
             mockValidator.Setup(x => x.IsValid(It.IsAny<string>())).Returns(true);
-
-            //mockValidator.Setup(x => x.LicenseKey).Returns(GetLicenseKeyExpriryString); //return from function example
 
             var sut = new CreditCardApplicationEvaluator(mockValidator.Object);
 
--- CreditCardApplications/CreditCardApplicationEvaluator.cs
+++ CreditCardApplications/CreditCardApplicationEvaluator.cs
@@ -46,33 +46,6 @@
 
             return CreditCardApplicationDecision.ReferredToHuman;
         }
-        /*
-        public CreditCardApplicationDecision EvaluateUsingOut(CreditCardApplication application)
-        {
-            if (application.GrossAnnualIncome >= HighIncomeThreshold)
-            {
-                return CreditCardApplicationDecision.AutoAccepted;
-            }
-            
-            validator.IsValid(application.FrequentFlyerNumber, out var isValidFrequentFlyerNumber);
-
-            if (!isValidFrequentFlyerNumber)
-            {
-                return CreditCardApplicationDecision.ReferredToHuman;
-            }
-
-            if (application.Age <= AutoReferralMaxAge)
-            {
-                return CreditCardApplicationDecision.ReferredToHuman;
-            }
-
-            if (application.GrossAnnualIncome < LowIncomeThreshold)
-            {
-                return CreditCardApplicationDecision.AutoDeclined;
-            }
-
-            return CreditCardApplicationDecision.ReferredToHuman;
-        }
-        */
+      
     }
 }
